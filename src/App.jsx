d0afe858896e9f044468fb7c5ae1622802623ea2
jessica4929 <jessica4929@gmail.com>
2024-05-22 13:46:41 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import {
+  HomeLayout,
+  About,
+  Landing,
+  Error,
+  Newsletter,
+  Cocktail,
+  SinglePageError,
+} from "./pages";
+import { loader as landingLoader } from "./pages/Landing";
+import { loader as singleCocktailLoader } from "./pages/Cocktail";
+import { action as newsletterAction } from "./pages/Newsletter";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+    },
+  },
+});
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        loader: landingLoader(queryClient),
+        element: <Landing />,
+        errorElement: <SinglePageError />,
+      },
+      {
+        path: "about",
+        element: <About />,
+      },
+      {
+        path: "cocktail/:id",
+        errorElement: <SinglePageError />,
+        loader: singleCocktailLoader(queryClient),
+        element: <Cocktail />,
+      },
+      {
+        path: "newsletter",
+        element: <Newsletter />,
+        action: newsletterAction,
+      },
+    ],
+  },
+]);
+const App = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+};
+export default App;
---
+++ src/pages/Landing.jsx
@@ -0,0 +1,42 @@
+import { useLoaderData } from "react-router-dom";
+import axios from "axios";
+import CocktailList from "./CocktailList";
+import SearchForm from "./SearchForm";
+
+const cocktailSearchUrl =
+  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+
+import { useQuery } from "@tanstack/react-query";
+
+const searchCocktailsQuery = (searchTerm) => {
+  return {
+    queryKey: ["search", searchTerm || "all"],
+    queryFn: async () => {
+      const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+      return response.data.drinks;
+    },
+  };
+};
+
+export const loader =
+  (queryClient) =>
+  async ({ request }) => {
+    const url = new URL(request.url);
+
+    const searchTerm = url.searchParams.get("search") || "";
+    // const response = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+    await queryClient.ensureQueryData(searchCocktailsQuery(searchTerm));
+    return { searchTerm: searchTerm };
+  };
+
+const Landing = () => {
+  const { searchTerm } = useLoaderData();
+  const { data: drinks } = useQuery(searchCocktailsQuery(searchTerm));
+  return (
+    <>
+      <SearchForm searchTerm={searchTerm} />
+      <CocktailList drinks={drinks} />
+    </>
+  );
+};
+export default Landing;
